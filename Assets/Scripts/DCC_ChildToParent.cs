0b89e1a999657df6a6625311f549d970d74af9f9
Programmer25X <denniscullipcross@icloud.com>
2025-12-07 22:58:00 +0000

    Platform Challenge Started

---
+++ Assets/Scripts/DCC_ChildToParent.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class DCC_ChildToParent : MonoBehaviour
+{
+    [SerializeField][Tooltip("Tag of the Parent Object")] private string _parentTag = "Moving";
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Physics.autoSyncTransforms = true;
+    }
+
+    private void OnTriggerEnter(Collider triggerHit)
+    {
+        if (triggerHit.gameObject.CompareTag(_parentTag))
+        {
+            transform.parent = triggerHit.gameObject.transform;
+            Debug.Log("Attached to parent object");
+        }
+    }
+
+    private void OnTriggerExit(Collider triggerHit)
+    {
+        if (triggerHit.gameObject.CompareTag(_parentTag))
+        {
+            transform.parent = null;
+            Debug.Log("Unattached to parent object");
+        }
+    }
+
+}

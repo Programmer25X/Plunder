191b613a8e14957c21bdd3e3194fd289f92543fb
Programmer25X <denniscullipcross@icloud.com>
2025-12-15 19:55:29 +0000

    Weighing Scale and Door system started

---
+++ Assets/DCC_Weighing_Scale.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+
+public class DCC_Weighing_Scale : MonoBehaviour
+{
+    private GameObject _door;
+
+    private Vector3 _closedDoorPosition = Vector3.zero;
+    private Vector3 _openDoorPosition = Vector3.zero;
+
+    [SerializeField] private bool isDoorOpen = true;
+
+    void Start()
+    {
+        _door = GameObject.Find("Door");
+
+        _closedDoorPosition = new Vector3(29.7000008f, 13.3500004f, 55.0f);
+        _openDoorPosition = new Vector3(29.7000008f, 6.1500001f, 55.0f);
+
+        _door.transform.position = _openDoorPosition;
+    }
+
+    void Update()
+    {
+        if (!isDoorOpen && _door.transform.position != _closedDoorPosition)
+        {
+            _door.transform.position = Vector3.Lerp(_door.transform.position, _closedDoorPosition, Time.deltaTime * 2);
+        }
+        else if (isDoorOpen && _door.transform.position != _openDoorPosition)
+        {
+            _door.transform.position = Vector3.Lerp(_door.transform.position, _openDoorPosition, Time.deltaTime * 2);
+        }
+    }
+
+    private void OnTriggerExit(Collider triggerHit)
+    {
+        if (triggerHit.gameObject.CompareTag("Gem_3"))
+        {
+            isDoorOpen = false;
+            Debug.Log("Door opening...");
+        }
+    }
+
+    private void OnTriggerEnter(Collider triggerHit)
+    {
+        if (triggerHit.gameObject.CompareTag("Gem_3"))
+        {
+            isDoorOpen = true;
+            Debug.Log("Door closing...");
+        }
+    }
+}

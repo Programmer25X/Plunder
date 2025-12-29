2a84f5a98cf9c92a4e067982634527e35ce68288
Programmer25X <denniscullipcross@icloud.com>
2025-12-29 17:16:15 +0000

    Started making improvements to play again menu

--- Assets/Scripts/DCC_OrbitIsland.cs
+++ Assets/Scripts/DCC_OrbitIsland.cs
@@ -2,13 +2,13 @@ using UnityEngine;
 
 public class DCC_OrbitIsland : MonoBehaviour
 {
-  [SerializeField] private GameObject _island;
+  [SerializeField] private GameObject _objectToOrbit;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
 
     // Update is called once per frame
     void Update()
     {
-        transform.RotateAround(_island.transform.position, Vector3.up, 20 * Time.deltaTime);
+        transform.RotateAround(_objectToOrbit.transform.position, Vector3.up, 20 * Time.deltaTime);
     }
 }

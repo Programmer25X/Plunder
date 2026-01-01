c830a3d8862df068a934abd9e49e767bbf3ad501
Programmer25X <denniscullipcross@icloud.com>
2026-01-01 19:55:07 +0000

    Organised project

--- Assets/Scripts/DCC_ArrowProjectile.cs
+++ Assets/Scripts/DCC_ArrowProjectile.cs
@@ -2,20 +2,20 @@ using UnityEngine;
 
 public class DCC_ArrowProjectile : MonoBehaviour
 {
-    private float distance = 50.0f;
-    private float speed = 25.0f;
-    private float damage = 25.0f;
+    private float _distance = 50.0f;
+    private float _speed = 25.0f;
+    private float _damage = 25.0f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        Destroy(gameObject, distance / speed);
-        GetComponent<Rigidbody>().linearVelocity = speed * transform.TransformDirection(Vector3.forward);
+        Destroy(gameObject, _distance / _speed);
+        GetComponent<Rigidbody>().linearVelocity = _speed * transform.TransformDirection(Vector3.forward);
     }
 
     private void OnCollisionEnter(Collision colliderHit)
     {
-        colliderHit.collider.gameObject.SendMessage("RecieveDamage", damage, SendMessageOptions.DontRequireReceiver);
+        colliderHit.collider.gameObject.SendMessage("RecieveDamage", _damage, SendMessageOptions.DontRequireReceiver);
         Destroy(gameObject); 
     }
 }
